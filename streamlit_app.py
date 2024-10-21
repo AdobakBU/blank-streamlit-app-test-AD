99a20a41ae330b0462f0ddfcfc2e9ec98f1330f0
AdobakBU <156374329+AdobakBU@users.noreply.github.com>
2024-10-21 09:51:48 -0400

    changed to GPT2 pipeline

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,10 @@
-import streamlit as st
-from openai import OpenAI
-import os
+from transformers import pipeline
 
-### Load your API Key
-my_secret_key = st.secrets['MyOpenAIKey']
-os.environ["OPENAI_API_KEY"] = my_secret_key
+### Create a GPT2 generator pipeline
+generator = pipeline('text-generation', model='gpt2')
 
-
-### Request the answer to the question "Damascus is a"
-client = OpenAI()
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "Complete the following prefix"},
-    {"role": "user", "content": "Damascus is a"}
-  ],
-  n=10,
-  max_tokens=20
-)
+### Generate the answer to the question "Damascus is a"
+generator("Damascus is a", max_length=20, num_return_sequences=10, truncation=True)
 
 ### Print all 10 completions:
 for i in range(10):

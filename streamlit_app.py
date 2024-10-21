8a2ea7b57a35b5d9fa18b5c980d0df28b82d8c5f
AdobakBU <156374329+AdobakBU@users.noreply.github.com>
2024-10-21 09:57:24 -0400

    added form

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 from transformers import pipeline
 
-### Create a GPT2 generator pipeline
-generator = pipeline('text-generation', model='gpt2')
+import streamlit as st
 
-### Generate the answer to the question "Damascus is a"
-generator("Damascus is a", max_length=20, num_return_sequences=10, truncation=True)
+with st.form("my_form"):
+    st.write("Please enter your values")
+    user_prompt = st.text_input("What is your prompt?")
+    user_length = st.text_input("Expected length of the response, in tokens?")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        generator = pipeline('text-generation', model='gpt2')
+        generator(user_prompt, max_length=user_length, num_return_sequences=10, truncation=True)
 
 ### Print all 10 completions:
 for i in range(10):
